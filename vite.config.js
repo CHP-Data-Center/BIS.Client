137dce16891aa051a63470893443622117b3f780
VuHPhuc <boycute2002@gmail.com>
2026-08-04 10:59:15 +0700

    feat: configure GitHub Pages deployment workflow and set dynamic base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Tự động nhận diện tên repository khi build trên GitHub Pages Actions
+const base = process.env.GITHUB_REPOSITORY
+  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+  : '/';
+
 export default defineConfig({
   plugins: [react()],
+  base: base,
   server: {
     port: 3000,
     open: true
   }
 })
+
